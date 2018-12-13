0101041986566448ceab07c1f6be8744e329456a
Yanuar <45526227+Yanuarramadhan@users.noreply.github.com>
2018-12-13 16:29:24 +0700

    CODING

---
+++ soal9.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+   int upah,jam;
+   cout<<"masukan jumlah jam kerjanya :";
+   cin>>jam;
+   upah=jam*5000;
+   cout<<"gaji karyawan adalah:"<<upah;
+   cin.get();}
